2373165f741a2e2144e904bafea7f7fa84201dbe
chinmay <chinmaypatil15@gmail.com>
2023-05-22 23:56:08 +0530

    questions

--- Ques5.py
+++ Ques5.py
@@ -1,9 +1,10 @@
+import matplotlib
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # Data visualization packages
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%matplotlib inline
+import matplotlib
 
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
@@ -55,8 +56,9 @@ def Clean_Text(data,column):
     #replace '
     data[column].replace('\'', ' ', regex=True, inplace=True)
     return data
-dataset=Clean_Text(df,'Text')
 
+dataset=Clean_Text(df,'Text')
+print(dataset)
 
 import nltk
 stopwords = set(nltk.corpus.stopwords.words('english')) | set(nltk.corpus.stopwords.words('french'))
@@ -65,6 +67,141 @@ dataset['Text'] = dataset['Text'].str.lower().apply(lambda x: ' '.join([word for
 dataset = dataset[dataset['Text']!= '']
 #reset data index
 dataset=dataset.reset_index().drop('index',axis=1)
+
+import nltk
+dataset['tokenized_sents'] = dataset.apply(lambda row: nltk.word_tokenize(row['Text']), axis=1)
+#remove words with less that 3 letters
+def cleaner(dataset):
+    for sentence in dataset.tokenized_sents:
+        for token in sentence:
+            if len(token) < 3  :
+                sentence.remove(token)
+    return dataset
+dataset=cleaner(dataset)
+print(dataset)
+
+#After successfully removing noise from our tokenze we detokenize the sentences
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+dataset['detokenized_sents'] = dataset.apply(lambda row: TreebankWordDetokenizer().detokenize(row['tokenized_sents']), axis=1)
+dataset=dataset[dataset['detokenized_sents'].str.len()>=4]
+print(dataset)
+
+#Dominant voccabulary in Darija
+#from wordcloud import WordCloud
+from wordcloud import WordCloud
+def plot_wordcloud(docx):
+    mywordcloud=WordCloud().generate(docx)
+    plt.figure(figsize=(20,10))
+    plt.imshow(mywordcloud,interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+#liste of English keywords
+language_list=dataset['Language'].unique().tolist()
+English_list=dataset[dataset['Language']=='English']['detokenized_sents'].tolist()
+English_docx=' '.join(English_list)
+plot_wordcloud(English_docx)
+
+#Dataset partition (trainning & testing data)
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X=dataset['detokenized_sents']
+y=dataset['Language']
+#we used  80% for training data and 20% for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(len(X_train))
+print(len(X_test))
+print(len(y_train))
+print(len(y_test))
+
+from sklearn.feature_extraction.text import CountVectorizer
+unigramVectorizer = CountVectorizer(analyzer='char', ngram_range=(1,1))
+X_unigram_train_raw = unigramVectorizer.fit_transform(X_train)
+X_unigram_test_raw = unigramVectorizer.transform(X_test)
+
+unigramFeatures = unigramVectorizer.get_feature_names()
+print('Number of unigrams in training set:', len(unigramFeatures))
+
+print(np.array(unigramFeatures))
+
+
+# Distribution of uni-grams through the laguages
+def train_lang_dict(X_raw_counts, y_train):
+    lang_dict = {}
+    for i in range(len(y_train)):
+        lang = y_train[i]
+        v = np.array(X_raw_counts[i])
+        if not lang in lang_dict:
+            lang_dict[lang] = v
+        else:
+            lang_dict[lang] += v
+
+    # to relative
+    for lang in lang_dict:
+        v = lang_dict[lang]
+        lang_dict[lang] = v / np.sum(v)
+
+    return lang_dict
+
+
+language_dict_unigram = train_lang_dict(X_unigram_train_raw.toarray(), y_train.values)
+
+
+# Collect relevant chars per language
+def getRelevantCharsPerLanguage(features, language_dict, significance=1e-4):
+    relevantCharsPerLanguage = {}
+   languages = ['French', 'English']
+    for lang in languages:
+        chars = []
+        relevantCharsPerLanguage[lang] = chars
+        v = language_dict[lang]
+        for i in range(len(v)):
+            if v[i] > significance:
+                chars.append(features[i])
+    return relevantCharsPerLanguage
+
+relevantCharsPerLanguage = getRelevantCharsPerLanguage(unigramFeatures, language_dict_unigram)
+languages=['French','English']
+
+relevantCharsPerLanguage = getRelevantCharsPerLanguage(unigramFeatures, language_dict_unigram)
+languages = ['French', 'English', 'Darija']
+
+
+# Print number of unigrams per language
+for lang in languages:
+    print(lang, len(relevantCharsPerLanguage[lang]))
+
+# get most common chars for a few European languages
+europeanLanguages = [ 'English', 'French', 'Darija']
+relevantChars_OnePercent = getRelevantCharsPerLanguage(unigramFeatures, language_dict_unigram, 1e-2)
+
+# collect and sort chars
+europeanCharacters = []
+for lang in europeanLanguages:
+    europeanCharacters += relevantChars_OnePercent[lang]
+europeanCharacters = list(set(europeanCharacters))
+europeanCharacters.sort()
+
+# build data
+indices = [unigramFeatures.index(f) for f in europeanCharacters]
+data = []
+for lang in europeanLanguages:
+    data.append(language_dict_unigram[lang][indices])
+
+#build dataframe
+df = pd.DataFrame(np.array(data).T, columns=europeanLanguages, index=europeanCharacters)
+df.index.name = 'Characters'
+df.columns.name = 'Languages'
+
+# plot heatmap
+import seaborn as sn
+import matplotlib.pyplot as plt
+sn.set(font_scale=0.8) # for label size
+sn.set(rc={'figure.figsize':(10, 10)})
+sn.heatmap(df, cmap="Greens", annot=True, annot_kws={"size": 12}, fmt='.0%')# font size
+plt.show()
 
 print(dataset)
 
